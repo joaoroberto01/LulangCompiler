56907101159384c6efa4eff5bc2117474df39ee3
joaoroberto <joaoroberto.gc.01@gmail.com>
2023-11-11 13:54:59 -0300

    - Remove excessive spacing on exception message

--- src/exceptions/CompilerException.java
+++ src/exceptions/CompilerException.java
@@ -4,6 +4,6 @@ import src.analyzers.Lexical;
 
 public class CompilerException extends RuntimeException {
     public CompilerException(String message) {
-        super(String.format("%s:%d:%d\nerror: %s\n\n", Lexical.filename, Lexical.lineCounter, Lexical.columnCounter, message));
+        super(String.format("%s:%d:%d\nerror: %s", Lexical.filename, Lexical.lineCounter, Lexical.columnCounter, message));
     }
 }
