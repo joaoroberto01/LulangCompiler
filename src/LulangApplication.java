541645185bf8ed37ee06437ffabab6383c5f902a
joaoroberto <joaoroberto.gc.01@gmail.com>
2023-12-04 22:26:19 -0300

    - comando se/enquanto nao estava verificando o tipo da expressao (deve ser booleano)

--- src/LulangApplication.java
+++ src/LulangApplication.java
@@ -1,13 +1,14 @@
 package src;
 
 import src.analyzers.Syntactic;
+import src.exceptions.CompilerException;
 
 import java.nio.file.NoSuchFileException;
 
 public class LulangApplication {
 
     public static void main(String[] args) {
-        String filepath = args.length == 0 ? "source_code.ll" : args[0];
+        String filepath = args.length == 0 ? "ricardo" : args[0];
         try {
             Syntactic.analyze(filepath);
 
@@ -15,8 +16,10 @@ public class LulangApplication {
             CodeGenerator.saveFile(filepath);
         } catch (NoSuchFileException e) {
             System.err.printf("file '%s' not found%n", filepath);
-        } catch (Exception e) {
+        } catch (CompilerException e) {
             System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println("internal compiler error: " + e.getMessage());
         }
 
     }
